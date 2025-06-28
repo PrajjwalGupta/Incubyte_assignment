bb44508606419e0b2bbf4cd95bcd28ae2dbd6162
PrajjwalGupta <prajjwalg786@gmail.com>
2025-06-28 23:14:31 +0530

    Initial Commit

---
+++ Incubyte_assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Incubyte_assignment
+//
+//  Created by Prajjwal on 28/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Incubyte_assignment/Incubyte_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Incubyte_assignmentApp.swift
+//  Incubyte_assignment
+//
+//  Created by Prajjwal on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Incubyte_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
